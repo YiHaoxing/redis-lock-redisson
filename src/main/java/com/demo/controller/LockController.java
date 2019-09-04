7435fc0b7ab3917ca2a56fc736e7cb5f114df936
yhx <m15670388498@163.com>
2019-09-05 01:44:40 +0800

    提交

--- src/main/java/com/demo/controller/LockController.java
+++ src/main/java/com/demo/controller/LockController.java
@@ -23,19 +23,22 @@ public class LockController {
     private RedisLockUtils redisLockUtils;
 
     /**
-     * 依次访问
-     * http://localhost:8080/thread1/T
-     * http://localhost:8080/thread2/T
-     * 首先thread1或先获取到锁.然后thread2的请求到达.
-     * 此时thread1在睡眠.因此还没有释放锁.thread2未能获取到锁.
-     * thread2获取锁时设置了5S等待时间.等待5S后,thread1已经释放了锁.此时thread2可以成功获取到锁.
+     * 打开两个窗口,访问t1后立刻访问t2
+     * http://localhost:8080/t1/T
+     * http://localhost:8080/t2/T
+     *
+     * 首先t1或先获取到锁.然后t2的请求到达.
+     * 此时t1在睡眠.因此还没有释放锁.t2未能获取到锁.
+     * t2获取锁时设置了5S等待时间.等待5S后,t1已经释放了锁.此时t2可以成功获取到锁.
+     * 假设t2获取锁设置的等待时间为1S.则等待时间到时,t1仍未释放锁.此时t2无法获取到锁.则获取锁失败.
      * 这就是tryLock()方法.
      */
-    @GetMapping("/thread1/{key}")
-    public String thread1(@PathVariable String key){
+    @GetMapping("/t1/{key}")
+    public String t1(@PathVariable String key){
+        boolean lock = false;
         try {
             //等待时间1S.缓存过期时间30S
-            boolean lock = redisLockUtils.getReentrantLock(key, 1, 30, TimeUnit.SECONDS);
+            lock = redisLockUtils.getReentrantLock(key, 1, 30, TimeUnit.SECONDS);
             if(lock){
                 //do something.
                 log.info("获取锁成功,Thread:{}",Thread.currentThread().getId());
@@ -47,16 +50,19 @@ public class LockController {
             e.printStackTrace();
         } finally {
             //释放锁
-            redisLockUtils.unlock(key);
-            log.info("释放锁,Thread:{}",Thread.currentThread().getId());
+            if(lock){
+                redisLockUtils.unlock(key);
+                log.info("释放锁,Thread:{}",Thread.currentThread().getId());
+            }
         }
-        return "thread1 over";
+        return "t1 over";
     }
-    @GetMapping("/thread2/{key}")
-    public String thread2(@PathVariable String key){
+    @GetMapping("/t2/{key}")
+    public String t2(@PathVariable String key){
+        boolean lock = false;
         try {
             //等待时间5S.缓存过期时间30S
-            boolean lock = redisLockUtils.getReentrantLock(key, 5, 30, TimeUnit.SECONDS);
+            lock = redisLockUtils.getReentrantLock(key, 5, 30, TimeUnit.SECONDS);
             if(lock){
                 //do something.
                 log.info("获取锁成功,Thread:{}",Thread.currentThread().getId());
@@ -68,43 +74,41 @@ public class LockController {
             e.printStackTrace();
         } finally {
             //释放锁
-            redisLockUtils.unlock(key);
-            log.info("释放锁,Thread:{}",Thread.currentThread().getId());
+            if(lock){
+                redisLockUtils.unlock(key);
+                log.info("释放锁,Thread:{}",Thread.currentThread().getId());
+            }
         }
-        return "thread2 over";
+        return "t2 over";
     }
 
 
     /**
-     * 依次访问
-     * http://localhost:8080/thread3/F
-     * http://localhost:8080/thread4/F
-     * thread3获取到锁后.然后thread4请求到达.
-     * thread3会睡眠10S,则thread4会一直阻塞直到成功获取锁.
-     * 这就是lock()方法.获取锁不可中断,线程会阻塞直到获取锁.
+     * 打开两个窗口,访问t3后立刻访问t4
+     * http://localhost:8080/t3/F
+     * http://localhost:8080/t4/F
+     * t3获取到锁后.t4请求到达.
+     * t3会睡眠10S,则t4会一直阻塞直到t3释放锁,t4成功获取锁为止.
+     * 这就是lock()方法.获取锁不可中断,线程会阻塞直到成功获取锁.
      */
-    @GetMapping("/thread3/{key}")
-    public String g1(@PathVariable String key){
+    @GetMapping("/t3/{key}")
+    public String g1(@PathVariable String key) throws InterruptedException {
         redisLockUtils.getLock(key, 30, TimeUnit.SECONDS);
         log.info("获取锁成功,Thread:{}",Thread.currentThread().getId());
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        Thread.sleep(10000);
         //释放锁
         redisLockUtils.unlock(key);
         log.info("释放锁,Thread:{}",Thread.currentThread().getId());
-        return "thread3 over";
+        return "t3 over";
     }
-    @GetMapping("/thread4/{key}")
+    @GetMapping("/t4/{key}")
     public String g2(@PathVariable String key){
         redisLockUtils.getLock(key, 30, TimeUnit.SECONDS);
         log.info("获取锁成功,Thread:{}",Thread.currentThread().getId());
         //释放锁
         redisLockUtils.unlock(key);
         log.info("释放锁,Thread:{}",Thread.currentThread().getId());
-        return "thread4 over";
+        return "t4 over";
     }
 
 }
